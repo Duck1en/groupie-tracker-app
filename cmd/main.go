461a16083f1ef54620d57d212760f1c64a56021f
Duck1en <catangel990@gmail.com>
2022-06-16 18:18:36 +0600

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"groupie-tracker/internal/delivery"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 
-	// server := delivery.New()
+	server := delivery.New()
 	fmt.Printf("Starting server at port :%v \n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, server.Router()))
 }
