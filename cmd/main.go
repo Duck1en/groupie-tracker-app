87ff84a7f54e50e6fb2f885cdc901f8a5ff8c024
rshezarr <rshezarr@gmail.com>
2022-06-20 20:42:20 +0600

    added timer for port scan

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"groupie-tracker/internal/delivery"
 )
@@ -14,10 +15,21 @@ func main() {
 
 	if port == "" {
 		fmt.Print("Enter port :")
-		fmt.Fscan(os.Stdin, &port)
-	}
+		ch := make(chan int)
 
-	server := delivery.New()
-	fmt.Printf("Starting server at port :%v \n", port)
-	log.Fatal(http.ListenAndServe(":"+port, server.Router()))
+		go func() {
+			fmt.Fscan(os.Stdin, &port)
+			ch <- 1
+		}()
+
+		select {
+		case <-ch:
+			server := delivery.New()
+			fmt.Printf("Starting server at port :%v \n", port)
+			log.Fatal(http.ListenAndServe(":"+port, server.Router()))
+			os.Exit(0)
+		case <-time.After(3 * time.Second):
+			log.Fatal("Time out")
+		}
+	}
 }
